95ca5ed1117879aef0c5ed2f804d26336d7630a2
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    Squashed commits: network traffic meter [1/2]
    [1/2] SystemUI: Network speed indicator
    
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009
    
    [1/2] SystemUI: Update network traffic indicator
    
    Android measures the amount of data transferred in bytes,
    however, by convention, data rates should be measured in bits
    per second.
    
    *Convert the change in total data and time to bits and seconds,
    and then divide the amount transferred by the number of
    seconds that have elapsed.
    
    *Update the labels and have them use the correct SI prefixes.
    
    *Factor out or add @hide to public methods and constructors
    
    *Remove all Runnables and Messages from Handler queue when
    disabled
    
    *Minor formatting changes to stay consistent with Android's
    code-style guidelines.
    https://source.android.com/source/code-style.html
    
    PS2: Set maximum digits to prevent it from filling the statusbar
    PS3: Rebase
    PS4: Add ability to display either bits or bytes
    
    Change-Id: I9add610a8d14f5a48f2af0a82ced0c186224e282
    
    base: fixed missing shading of statusbar network traffic
    
    http://jira.omnirom.org/browse/OMNI-326
    
    PS4:
    handle traffic like signal cluster
    
    Change-Id: I10c0f0fd1d63e78c9a1d09d1bff6a5221b8adec5
    
    [1/2] Base: Upgrade network traffic meter
    
    *Updated class name to be less ambiguous
    *Replaced the multi-check in settings with a proper listview
    *Added the ability to view up-stream, down-stream or both
    *Added the option to change how often it refreshes
    *Added a check before it updates the status bar to prevent it from
    refreshing everything needlessly
    *Added icons to show data direction
    *Remove unused style attributes
    
    Icon artwork is courtesy android design downloads
    Some features were inspired by previous work done by 0xD34D
    
    OmniGears: https://gerrit.omnirom.org/#/c/5130/
    
    Change-Id: I9f86292a6adbc5e30967df76fae510be248fc38b
    
    Add the ability to enable or disable the arrows networks directions
    
    Inspired from omnirom https://github.com/omnirom/android_frameworks_base/commit/9adbe0ff56cbf7edf6da61b1a59b9d8ddf169e29
    
    Change-Id: I441f59e7fa4f37536f9c5317126ec056a8438132
    
    Conflicts:
    	packages/SystemUI/res/values/spirit_dimens.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3418,7 +3418,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         @Override
         public void run() {
             checkBarModes();
-        }};
+        }
+    };
 
     @Override
     public void setInteracting(int barWindow, boolean interacting) {
