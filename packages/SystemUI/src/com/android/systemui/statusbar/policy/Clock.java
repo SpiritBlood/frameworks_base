03b520074e27c64ee473f19a8cd5693a1f2f4b65
Daniel MM <daxx4182@gmail.com>
2014-01-19 18:51:00 -0500

    Status Bar Color [1/2] Change-Id: I4f7d0f7d1ee49910e7f506a682b3d87462807923

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -95,6 +95,9 @@ public class Clock extends TextView implements DemoMode {
 
     private SettingsObserver mSettingsObserver;
 
+    private boolean mCustomColor;
+    private int systemColor;
+
     protected class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
             super(handler);
@@ -125,6 +128,12 @@ public class Clock extends TextView implements DemoMode {
                     this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.STATUSBAR_CLOCK_FONT_STYLE), false,
+                    this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.CUSTOM_SYSTEM_ICON_COLOR), false,
+                    this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SYSTEM_ICON_COLOR), false,
                     this, UserHandle.USER_ALL);
             updateSettings();
         }
@@ -378,9 +387,16 @@ public class Clock extends TextView implements DemoMode {
                 Settings.System.STATUSBAR_CLOCK_FONT_STYLE, FONT_NORMAL,
                 UserHandle.USER_CURRENT);
 
+        mCustomColor = Settings.System.getIntForUser(resolver,
+                Settings.System.CUSTOM_SYSTEM_ICON_COLOR, 0,
+                UserHandle.USER_CURRENT) == 1;
+
         int defaultColor = getResources().getColor(R.color.status_bar_clock_color);
         int clockColor = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUSBAR_CLOCK_COLOR, defaultColor,
+                UserHandle.USER_CURRENT);
+        int systemColor = Settings.System.getIntForUser(resolver,
+                Settings.System.SYSTEM_ICON_COLOR, defaultColor,
                 UserHandle.USER_CURRENT);
         if (clockColor == Integer.MIN_VALUE) {
             // flag to reset the color
@@ -388,7 +404,11 @@ public class Clock extends TextView implements DemoMode {
         }
 
         if (mAttached) {
-            setTextColor(clockColor);
+            if (mCustomColor) {
+                setTextColor(systemColor);
+            } else {
+                setTextColor(clockColor);
+            }
             getFontStyle(mClockFontStyle);
             updateClockVisibility();
             updateClock();
