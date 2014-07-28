bbb06927d0dafb924071a9663ee4a59ee623d7a4
Nico60 <ndhouailly@gmail.com>
2014-07-28 18:10:05 +0200

    Fix theme not applying correctly with quicksettings tiles text color (1/2)
    Change-Id: Id50fcdfe950fdc7989ce23a83c3236466323c6fb

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4638,9 +4638,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     || uri.equals(Settings.System.getUriFor(
                         Settings.System.QUICK_TILES_BG_PRESSED_COLOR))
                     || uri.equals(Settings.System.getUriFor(
-                        Settings.System.QUICK_TILES_TEXT_COLOR))
-                    || uri.equals(Settings.System.getUriFor(
-                        Settings.System.QUICK_TILES_BG_ALPHA))) {
+                        Settings.System.QUICK_TILES_TEXT_COLOR))) {
                 if (mSettingsContainer != null) {
                     mQS.setupQuickSettings();
                 }
@@ -4683,10 +4681,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
             cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.QUICK_TILES_TEXT_COLOR),
-                    false, this, UserHandle.USER_ALL);
-
-            cr.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.QUICK_TILES_BG_ALPHA),
                     false, this, UserHandle.USER_ALL);
 
             cr.registerContentObserver(
