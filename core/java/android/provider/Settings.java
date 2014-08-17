3362274bb7bb7132ea410937909f436dd641fca7
Nico60 <ndhouailly@gmail.com>
2014-08-17 17:59:21 +0200

    Revert "FWB: Option to hide weather icon in statusbar (1/2)"
    This reverts commit cbcd6db211e82834933d257a54d9805df20edfdf.
    
    Change-Id: I36730f1d0eb89045ddd9dcaa73ce866af43801d3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4696,11 +4696,6 @@ public final class Settings {
          */
         public static final String SYSTEMUI_WEATHER_NOTIFICATION = "cfx_weather_notification";
 
-	/**
-	* @hide
-	*/
-	public static final String SYSTEMUI_WEATHER_ICON = "cfx_weather_icon";
-
        /**
         *
         * @hide
