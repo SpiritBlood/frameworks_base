b483243d934e58bbd2aa0bd9417695f2e3368856
cretin45 <cretin45@gmail.com>
2014-10-21 13:33:58 -0700

    SystemUI: Status bar UI cleanup for MSIM and roaming
    Change-Id: Id0a94962ff062daeba1988ccf799ca1f8c3e2e1d
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkController.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -352,8 +352,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private SharedPreferences mShared;
 
     // carrier/wifi label
+    private View mCarrierLableContainer;
     private TextView mCarrierLabel;
-    private TextView mSubsLabel;
+    private View mSubsLabel;
     private TextView mWifiLabel;
     private View mWifiView;
     private View mCarrierAndWifiView;
@@ -1359,6 +1360,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         setAreThereNotifications();
 
         // Other icons
+        mCarrierLableContainer = mStatusBarWindow.findViewById(R.id.carrier_label_container);
         if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
             mMSimNetworkController = new MSimNetworkController(mContext);
             mMSimSignalClusterView = (MSimSignalClusterView)
@@ -1388,7 +1390,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mCarrierAndWifiView = mStatusBarWindow.findViewById(R.id.carrier_wifi);
             mWifiView = mStatusBarWindow.findViewById(R.id.wifi_view);
             mCarrierLabel = (TextView)mStatusBarWindow.findViewById(R.id.carrier_label);
-            mSubsLabel = (TextView)mStatusBarWindow.findViewById(R.id.subs_label);
+            mSubsLabel = mStatusBarWindow.findViewById(R.id.subs_label);
+            int numPhones = MSimTelephonyManager.getDefault().getPhoneCount();
+            if (numPhones == 3) {
+                mSubsLabel.findViewById(R.id.sub2_separator).setVisibility(View.VISIBLE);
+                mSubsLabel.findViewById(R.id.sub3_label).setVisibility(View.VISIBLE);
+            }
             mShowCarrierInPanel = (mCarrierLabel != null);
 
             if (DEBUG) Log.v(TAG, "carrierlabel=" + mCarrierLabel + " show=" +
@@ -2388,11 +2395,16 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (!mShowCarrierInPanel || mCarrierAndWifiView == null) {
             return;
         }
+        final boolean isMultiSim =  MSimTelephonyManager.getDefault().isMultiSimEnabled();
+        int labelHeight = mCarrierAndWifiViewHeight;
+        if (isMultiSim) {
+            labelHeight = labelHeight * 2; //SubLabel height is same a carrier label
+        }
         // The idea here is to only show the carrier label when there is enough room to see it,
         // i.e. when there aren't enough notifications to fill the panel.
         if (SPEW) {
             Log.d(TAG, String.format("pileh=%d scrollh=%d carrierh=%d",
-                    mPile.getHeight(), mScrollView.getHeight(), mCarrierAndWifiViewHeight));
+                    mPile.getHeight(), mScrollView.getHeight(), labelHeight));
         }
 
         final boolean emergencyCallsShownElsewhere = mEmergencyCallLabel != null;
@@ -2403,7 +2415,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         final boolean makeVisible =
             !(emergencyCallsShownElsewhere && isEmergencyOnly)
             && mPile.getHeight() <
-            (mNotificationPanel.getHeight() - mCarrierAndWifiViewHeight
+            (mNotificationPanel.getHeight() - labelHeight
                 - mNotificationHeaderHeight - calculateCarrierLabelBottomMargin())
             && mScrollView.getVisibility() == View.VISIBLE
             && !mAnimatingFlip;
@@ -2413,11 +2425,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             if (DEBUG) {
                 Log.d(TAG, "making carrier label " + (makeVisible?"visible":"invisible"));
             }
-            mCarrierAndWifiView.animate().cancel();
+            mCarrierLableContainer.animate().cancel();
             if (makeVisible) {
-                mCarrierAndWifiView.setVisibility(View.VISIBLE);
+                mCarrierLableContainer.setVisibility(View.VISIBLE);
             }
-            mCarrierAndWifiView.animate()
+            mCarrierLableContainer.animate()
                 .alpha(makeVisible ? 1f : 0f)
                 //.setStartDelay(makeVisible ? 500 : 0)
                 //.setDuration(makeVisible ? 750 : 100)
@@ -2426,8 +2438,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         if (!mCarrierAndWifiViewVisible) { // race
-                            mCarrierAndWifiView.setVisibility(View.INVISIBLE);
-                            mCarrierAndWifiView.animate().alpha(0f);
+                            mCarrierLableContainer.setVisibility(View.INVISIBLE);
+                            mCarrierLableContainer.setAlpha(0f);
                         }
                     }
                 })
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -179,6 +179,7 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
     String mLastCombinedLabel = "";
 
     protected boolean mHasMobileDataFeature;
+    protected String mEmergencyCallOnlyLabel;
     private boolean mUseSixBar;
     private DirtyObserver mObserver = null;
 
@@ -248,6 +249,8 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
     public NetworkController(Context context) {
         mContext = context;
         mCr = context.getContentResolver();
+        mEmergencyCallOnlyLabel = mContext.getString(com.android.internal.R.string
+                .emergency_calls_only);
         final Resources res = context.getResources();
 
         // Register settings observer and set initial preferences
@@ -314,6 +317,13 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
         mObserver.observe();
 
         mLastLocale = mContext.getResources().getConfiguration().locale;
+        initNetworkState();
+    }
+
+    protected void initNetworkState() {
+        TelephonyManager tm = TelephonyManager.getDefault();
+        updateNetworkName(true, tm.getSimOperatorName(),
+                    true, tm.getNetworkOperatorName());
     }
 
     public boolean hasMobileDataFeature() {
@@ -481,6 +491,7 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
     @Override
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
+        Log.d(TAG, "onReceive() " + action);
         if (action.equals(WifiManager.RSSI_CHANGED_ACTION)
                 || action.equals(WifiManager.WIFI_STATE_CHANGED_ACTION)
                 || action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
@@ -995,19 +1006,24 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
     }
 
     void updateNetworkName(boolean showSpn, String spn, boolean showPlmn, String plmn) {
-        if (false) {
+        if (DEBUG) {
             Log.d("CarrierLabel", "updateNetworkName showSpn=" + showSpn + " spn=" + spn
                     + " showPlmn=" + showPlmn + " plmn=" + plmn);
         }
         StringBuilder str = new StringBuilder();
         boolean something = false;
         if (showPlmn && plmn != null) {
+            plmn = maybeStripPeriod(plmn);
             str.append(plmn);
             something = true;
         }
-        if (showSpn && spn != null) {
+        if (showSpn && spn != null &&
+                !spn.equals(plmn) &&
+                !mEmergencyCallOnlyLabel.equals(plmn)) {
             if (something) {
+                str.append("  ");
                 str.append(mNetworkNameSeparator);
+                str.append("  ");
             }
             str.append(spn);
             something = true;
@@ -1017,6 +1033,16 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
         } else {
             mNetworkName = mNetworkNameDefault;
         }
+        mNetworkName = maybeStripPeriod(mNetworkName);
+    }
+
+    protected String maybeStripPeriod(String name) {
+        if (!TextUtils.isEmpty(name)) {
+            return name.equals(mNetworkNameDefault) ?
+                    name.replace(".", "") :
+                    name;
+        }
+        return name;
     }
 
     // ===== Wifi ===================================================================
