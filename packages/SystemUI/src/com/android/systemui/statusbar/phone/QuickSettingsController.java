DirtyUnicorns/android_frameworks_base

Dynamic Color Changer Tile [1/2]
To do list
- Add QS flip animation
- Add longpres to switch between screen color and action bar color

PS2 - Added QS flip animation
PS3 - Added ability to switch between screen color and action bar color
PS4 - Fixed mdpi icon (photoshop made it look like shit)
PS5 - Fixed other icons as well

Change-Id: I0069940cd8ed3fdc674b908746f6710b672aaac1

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -66,6 +66,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_HEADSUP;
 import static com.android.internal.util.slim.QSConstants.TILE_CAMERA;
 import static com.android.internal.util.slim.QSConstants.TILE_CPUFREQ;
 import static com.android.internal.util.slim.QSConstants.TILE_DIRTYTWEAKS;
+import static com.android.internal.util.slim.QSConstants.TILE_DYNAMICCOLORS;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -131,6 +132,7 @@ import com.android.systemui.quicksettings.HeadsupTile;
 import com.android.systemui.quicksettings.CameraTile;
 import com.android.systemui.quicksettings.CPUFreqTile;
 import com.android.systemui.quicksettings.DirtyTweaksTile;
+import com.android.systemui.quicksettings.DynamicColorsTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -306,6 +308,8 @@ public class QuickSettingsController {
                 qs = new DirtyTweaksTile(mContext, this);
             } else if (tile.equals(TILE_HEADSUP)) {
                 qs = new HeadsupTile(mContext, this);
+            } else if (tile.equals(TILE_DYNAMICCOLORS)) {
+                qs = new DynamicColorsTile(mContext, this);
             } else if (tile.contains(TILE_CPUFREQ)) {
                 if (cpufreqSupported) {
                     qs = new CPUFreqTile(mContext, this);

SpiritBlood/frameworks_base

Conflicts:
	core/java/com/android/internal/util/slim/QSConstants.java
	packages/SystemUI/res/values/strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -60,6 +60,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_HOVER;
 import static com.android.internal.util.cm.QSConstants.TILE_GESTUREPANEL;
 import static com.android.internal.util.cm.QSConstants.TILE_FCHARGE;
 import static com.android.internal.util.cm.QSConstants.TILE_SPIRIT;
+import static com.android.internal.util.cm.QSConstants.TILE_DYNAMICCOLORS;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -128,6 +129,7 @@ import com.android.systemui.quicksettings.HoverTile;
 import com.android.systemui.quicksettings.GesturePanelTile;
 import com.android.systemui.quicksettings.FastChargeTile;
 import com.android.systemui.quicksettings.SpiritControlsTile;
+import com.android.systemui.quicksettings.DynamicColorsTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -336,6 +338,8 @@ public class QuickSettingsController {
                 qs = new ScreenshotTile(mContext, this, mHandler);
 	    } else if (tile.equals(TILE_POWER)) {
                 qs = new PowerMenuTile(mContext, this);
+            } else if (tile.equals(TILE_DYNAMICCOLORS)) {
+                qs = new DynamicColorsTile(mContext, this);
             } else if (tile.contains(TILE_CPUFREQ)) {
                 if (cpufreqSupported) {
                     qs = new CPUFreqTile(mContext, this);